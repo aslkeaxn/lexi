e91d9c46f5e624f4d45f2eb1cd5683f922565309
aslkeaxn <69468068+aslkeaxn@users.noreply.github.com>
2024-02-03 17:16:26 +0300

    this sucks lol

--- server/src/constant.ts
+++
@@ -1,7 +0,0 @@
-export const constant = {
-  userRoute: "/api/users",
-  projectRoute: "/api/projects",
-  namespaceRoute: "/api/namespaces",
-  groupRoute: "/api/groups",
-  textRoute: "/api/texts",
-};
--- server/src/utils/get-req.ts
+++
@@ -1,9 +0,0 @@
-import supertest from "supertest";
-import { createApp } from "../app";
-import { TDatabase } from "../db";
-
-export function req(db: TDatabase) {
-  const app = createApp(db);
-  const req = supertest(app);
-  return req;
-}
